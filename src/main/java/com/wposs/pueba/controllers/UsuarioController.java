dc207acfb19470940237a509342e8071e724f2a8
Xxarhabia <msarabiacarrascal@gmail.com>
2023-06-24 12:14:22 -0500

    funciones controller

--- src/main/java/com/wposs/pueba/controllers/UsuarioController.java
+++ src/main/java/com/wposs/pueba/controllers/UsuarioController.java
@@ -1,5 +1,8 @@
 package com.wposs.pueba.controllers;
 
+import com.wposs.pueba.entities.UsuarioEntity;
+import com.wposs.pueba.services.impl.UsuarioServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,13 +10,23 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class UsuarioController {
 
+    @Autowired
+    private UsuarioServiceImpl usuarioService;
+
     @GetMapping("/")
     public String inicio(){
         return "index";
     }
 
-    @PostMapping("/agregar")
+    @GetMapping("/agregar")
     public String agregar(){
         return "register";
     }
+
+    @PostMapping("/guardar")
+    public String guardar(UsuarioEntity usuario){
+        usuarioService.agregarUsuario(usuario);
+        return "redirect:/";
+    }
+
 }
